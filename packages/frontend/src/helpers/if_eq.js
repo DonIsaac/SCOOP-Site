b71707e0812c9e33148ecf46b85c9eb5c1a7ad91
Don Isaac <donald.isaac@gmail.com>
2019-07-15 20:54:14 -0400

    Nav partial is working and sexy

--- packages/frontend/src/helpers/if_eq.js
+++ packages/frontend/src/helpers/if_eq.js
@@ -1,4 +1,9 @@
 module.exports.if_eq = function if_eq(a, b, opts) {
+  console.log(a, b)
+  if (typeof a === 'string')
+    a = a.trim();
+  if (typeof b === 'string')
+    b = b.trim();
   if (a == b) {
     return opts.fn(this);
   } else {
