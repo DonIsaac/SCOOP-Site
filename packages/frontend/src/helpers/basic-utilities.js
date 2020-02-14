12acc1f563915a65e2405fa04b25d5415d685f2d
Emery Bacon <emery390@gmail.com>
2020-02-14 00:55:47 -0500

    Added new orgs from the google doc to the members page. Changed 'var' to 'let' in basic-utilities.js

--- packages/frontend/src/helpers/basic-utilities.js
+++ packages/frontend/src/helpers/basic-utilities.js
@@ -3,7 +3,6 @@
  * value of one variable is equal to the value of another variable
  */
 module.exports.if_eq = function if_eq(a, b, opts) {
-  console.log(a, b)
   if (typeof a === 'string')
     a = a.trim();
   if (typeof b === 'string')
@@ -19,8 +18,8 @@ module.exports.if_eq = function if_eq(a, b, opts) {
  * similar to Integer#times in ruby
  */
 module.exports.times = function times(n, block) {
-  var accum = '';
-  for (var i = 0; i < n; i ++)
+  let accum = '';
+  for (let i = 0; i < n; i ++)
     accum += block.fn(i);
   return accum;
 }
