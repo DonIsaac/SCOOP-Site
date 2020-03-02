a17b4305928244984e23f169c621fb67a99b3876
Emery Bacon <emery390@gmail.com>
2020-03-02 15:27:31 -0500

    Fixed the calendar to be an embedded version of both the events and meetings calendars

--- packages/frontend/src/assets/js/index.js
+++ packages/frontend/src/assets/js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   console.log('Hey you, you\'re finally awake');
 
-  $.ajax({
-    method: 'GET',
-    crossDomain: true,
-    // dataType: 'text/ical',
-    url: 'https://calendar.google.com/calendar/ical/0f163qlkupueof5qse9r78gdj8%40group.calendar.google.com/public/basic.ics'
-  })
-  .done(data => {
-    console.log(data)
-  })
+  // $.ajax({
+  //   method: 'GET',
+  //   crossDomain: true,
+  //   // dataType: 'text/ical',
+  //   url: 'https://calendar.google.com/calendar/b/0/embed?height=600&amp;wkst=1&amp;bgcolor=%23172410&amp;ctz=America%2FNew_York&amp;src=c2Nvb3B1bWRAZ21haWwuY29t&amp;color=%23D50000'
+  // })
+  // .done(data => {
+  //   console.log(data)
+  // })
 })
